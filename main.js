4a928e35e718893f19b6c4ca279248f358d9d556
Lynerd <tilleryl10@gmail.com>
2023-10-31 16:28:46 -0400

    no idea where to start

---
+++ main.js
@@ -0,0 +1,8 @@
+const gitAPI = "https://api,github.com/users/lynerdt";
+const reposiAPI = "https://api.github.com/users/lynerdt/repos";
+let profile = document.getElementById("profile");
+let profileinfo = document.getElementById("profile-info");
+
+fetch(gitAPI).then((repsonse) => {
+  return Response.json();
+});
